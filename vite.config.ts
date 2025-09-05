e8075d16ff0e6577014c029e2d5cbc9b72011aef
thiraviyalingesh <lingesh559@gmail.com>
2025-09-05 22:15:34 +0530

    mupdf-added-base-version

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    topLevelAwait({
+      promiseExportName: "__tla",
+      promiseImportName: i => `__tla_${i}`
+    })
+  ],
   build: {
-    target: 'esnext'
+    target: 'es2022',
+    rollupOptions: {
+      output: {
+        format: 'es'
+      }
+    }
+  },
+  esbuild: {
+    target: 'es2022'
   },
   optimizeDeps: {
-    include: ['pdfjs-dist']
+    exclude: ['mupdf'],
+    esbuildOptions: {
+      target: 'es2022',
+      supported: {
+        "top-level-await": true
+      }
+    }
+  },
+  server: {
+    fs: {
+      allow: ['..', 'node_modules']
+    }
+  },
+  assetsInclude: ['**/*.wasm'],
+  define: {
+    global: 'globalThis'
   }
 })
